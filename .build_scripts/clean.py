b0042cf38aecf32c86e6976e36951c8312875444
Russ Biggs <russbiggs@gmail.com>
2019-09-24 16:48:50 +0700

    build fixes

--- .build_scripts/clean.py
+++ .build_scripts/clean.py
@@ -1,14 +1,82 @@
 import os
-import re
-import shutil
-import pathlib
-
-import frontmatter
 import toml
+import pathlib
+import shutil
+import frontmatter
+import re
 
+from typing import List, Dict
 
+def copy_parents(src, dest_folder, dir_offset=0):
+    prev_offset = 0 if dir_offset == 0 else src.replace('/', '%', dir_offset - 1).find('/') + 1
+    post_offset = src.rfind('/')
+    src_dirs = '' if post_offset == -1 else src[prev_offset:post_offset]
+    src_filename = src[post_offset + 1:]
+    os.makedirs(f'{dest_folder}/{src_dirs}', exist_ok=True)
+    shutil.copy(src, f'{dest_folder}/{src_dirs}/{src_filename}')
+
+def move_images(dir = ""):
+    for path, _, files in os.walk(f'static/{dir + "/"}'):
+        for filename in files:
+            ext = os.path.splitext(filename)[1]
+            if ext.lower() in ['.jpg','.jpeg','.png','.gif']:
+                copy_parents(os.path.join(path, filename), 'pdf-build', 1)
+
+def clean_markdown(filename):
+    post = frontmatter.load(filename)
+    if post.content:
+        title = post.metadata.get('title', '')
+        guide = {}
+        guide['filename'] = filename
+        content = post.content
+        replace_regex = r'(!\[[^\]]*\]\()(\/)(.*?)\s*("(?:.*[^"])")?\s*(\))'
+        content = re.sub(replace_regex, r'\1\3\5', content)
+        guide['content'] = ''
+        if title:
+            guide['content'] += '# {0} \n\n'.format(title)
+        guide['content'] += content
+        return guide['content']
+    return ''
+
+def traverse_pages(page_path) -> List:
+    pdf_pages = []
+    for path, _, files in os.walk(page_path):
+        p = pathlib.Path(path)
+        for filename in files:
+            ext = os.path.splitext(filename)[1]
+            if ext == '.md':
+                out_path = p.parts[1]
+                pdf_markdown = pathlib.Path('pdf-build',out_path, filename)
+                if not os.path.exists(os.path.dirname(pdf_markdown)):
+                    try:
+                        os.makedirs(os.path.dirname(pdf_markdown))
+                    except OSError as exc: 
+                        if exc.errno != errno.EEXIST:
+                            raise
+                content = clean_markdown(pathlib.Path(path, filename))
+                if content:
+                    pdf_pages.append({"content":content, "name": filename})
+                    with open(pdf_markdown, 'w') as f:
+                        f.write(content)
+    return pdf_pages
+
+def build_full_site(pages: List[Dict], site_name: str, lang: str = ''):
+    full_pdf_content = "\n\n\pagebreak\n\n"
+    lang_path = ''
+    if lang:
+        lang_path = f'{lang}/'
+    content = clean_markdown(f"content/{lang_path}_index.md")
+    if content:
+        full_pdf_content += content
+        full_pdf_content += "\n\n\pagebreak\n\n"
+    for item in sorted(pages, key=lambda k: k['name']):
+        full_pdf_content += item['content']
+        full_pdf_content += "\n\n\pagebreak\n\n"
+    with open(f'pdf-build/{lang_path}{site_name}.fullsite.md', 'w') as f:
+        f.write(full_pdf_content)
 
 def main():
+    langs = []
     extensions = {}
     with open('config.toml', 'r') as f:
         config = toml.loads(f.read())
@@ -20,117 +88,18 @@ def main():
         site_name = site_name.replace('.', '')
         if 'languages' in config:
             for key in config['languages']:
+                langs.append(key)
                 extensions[config['languages'][key]['contentDir'].split('/')[-1]] = key
-                if key != default_lang:
-                    os.mkdir(os.path.join('pdf-build', key))
-    guides = []
-    if 'pages' in os.listdir('content'):
-        # single language
-        contents = []
-        for item in os.listdir('content'):
-            if os.path.isfile(os.path.join('content',item)):
-                ext = os.path.splitext(item)[1]
-                if ext != '.md':
-                    shutil.copyfile(os.path.join('content', item), os.path.join('pdf-build',item))
-        # move image files
-        for path, _, files in os.walk('content/pages'):
-            for filename in files:
-                ext = os.path.splitext(filename)[1]
-                if ext != '.md':
-                    shutil.copyfile(os.path.join(path, filename), os.path.join('pdf-build',filename))
-        for path, _, files in os.walk('static/'):
-            p = pathlib.Path(path)
-            cleaned_path = p.relative_to(*p.parts[:2])
-            pdf_path = pathlib.Path('pdf-build').joinpath(p.relative_to(*p.parts[:2]))
-            if not os.path.exists(pdf_path):
-                os.makedirs(pdf_path)
-            for filename in files:
-                ext = os.path.splitext(filename)[1]
-                if ext.lower() in ['.jpg','.jpeg','.png','.gif']:
-                    copy_parents(os.path.join(path, filename),'pdf-build', 1)
-            images = []
-        for path, d, f in os.walk('pdf-build'):
-            p = pathlib.Path(*pathlib.Path(path).parts[1:])
-            for file in f:
-                if re.search(r'.*\.(jpe?g|png|gif)$', file):
-                    images.append(os.path.join(p, file))
-        for path, _, files in os.walk('content/pages'):
-            for filename in files:
-                ext = os.path.splitext(filename)[1]
-                if ext == '.md':
-                    content = clean_markdown(path, filename, images, default_lang=default_lang)
-                    if content:
-                        contents.append({'name':filename, 'content': content})
-                else:
-                    continue
-        full_pdf_content = "\n\n\pagebreak\n\n"
-        content = clean_markdown("content", "_index.md", images)
-        if content:
-            full_pdf_content += content
-            full_pdf_content += "\n\n\pagebreak\n\n"
-        for item in sorted(contents, key=lambda k: k['name']):
-            full_pdf_content += item['content']
-            full_pdf_content += "\n\n\pagebreak\n\n"
-        with open('pdf-build/' + site_name + ".fullsite.md", 'w') as f:
-            f.write(full_pdf_content)
-        
+                os.mkdir(os.path.join('pdf-build', key))
+    if langs:
+        for lang in langs:
+            move_images(lang)
+            pdf_pages = traverse_pages(f'content/{lang}/pages')
+            build_full_site(pdf_pages, site_name, lang)
     else:
-        # multiple languages
-        for lang in os.listdir('content'):
-            contents = []
-            lang_key = extensions[lang]
-            lang_path = os.path.join('content', lang)
-            for path, _, files in os.walk(lang_path):
-                if lang_key == default_lang:
-                    lang_key = ''
-                for filename in files:
-                    ext = os.path.splitext(filename)[1]
-                    if ext == '.md':
-                        content = clean_markdown(path, filename, lang_key, default_lang)
-                        if content:
-                            contents.append({'name':filename, 'content': content})
-                    else:
-                        shutil.copyfile(os.path.join(path,filename), os.path.join('pdf-build',lang_key,filename))
-            full_pdf_content = "\n\n\pagebreak\n\n"
-            for item in sorted(contents, key=lambda k: k['name']):
-                full_pdf_content += item['content']
-                full_pdf_content += "\n\n\pagebreak\n\n"
-            with open('pdf-build/' + site_name + ".md", 'w') as f:
-                f.write(full_pdf_content)
-
-def copy_parents(src, dest_folder, dir_offset=0):
-    prev_offset = 0 if dir_offset == 0 else src.replace('/', '%', dir_offset - 1).find('/') + 1
-    post_offset = src.rfind('/')
-
-    src_dirs = '' if post_offset == -1 else src[prev_offset:post_offset]
-    src_filename = src[post_offset + 1:]
-
-    os.makedirs(f'{dest_folder}/{src_dirs}', exist_ok=True)
-    shutil.copy(src, f'{dest_folder}/{src_dirs}/{src_filename}')
-
-
-def clean_markdown(path, filename, images, lang="", default_lang = "en"):
-    post = frontmatter.load(os.path.join(path, filename))
-    if post.content:
-        title = post.metadata.get('title', '')
-        guide = {}
-        guide['filename'] = filename
-        content = post.content
-        for image in images:
-            replace_regex = r'(\!\[.*\]).*(\()(.*\/)(' + re.escape(str(image)) + r')([A-Za-z\s\"\'\-\,\.\;\:]*)(\))'
-            content = re.sub(replace_regex, r'\1\2\4\5\6', content)
-        guide['content'] = ''
-        if title:
-            guide['content'] += '# {0} \n\n'.format(title)
-        guide['content'] += content
-        if lang and lang != default_lang:
-            out_file = os.path.join('pdf-build', lang, guide['filename'])
-        else: 
-            out_file = os.path.join('pdf-build', guide['filename'])
-        with open(out_file, 'w') as f:
-            f.write(guide['content'])
-        return guide['content']
-    return ''
+        move_images()
+        pdf_pages = traverse_pages(f'content/pages')
+        build_full_site(pdf_pages, site_name)
 
 if __name__ == "__main__":
     main()
--- .build_scripts/pandoc.sh
+++ .build_scripts/pandoc.sh
@@ -1,10 +1,17 @@
 cd $HOME/pdf-build
-ls . 
 mkdir -p $HOME/public/pdfs
 mkdir -p $HOME/tmp
 for d in $(find . -mindepth 1 -type d); do
     mkdir $HOME/public/pdfs/"$d"
+    mkdir $HOME/tmp/"$d"
 done &&
+
+for i in $(find . -mindepth 1); do
+    if [ -f "$i" ] && [ "${i: -3}" == ".md" ]; then
+        pdfreplace=${i//\.md/.pdf}
+        pdffilename=${pdfreplace/\.\//}
+    fi
+done
 
 for i in $(find . -mindepth 1); do
     if [ -f "$i" ] && [ "${i: -3}" == ".md" ]; then
@@ -15,8 +22,8 @@ for i in $(find . -mindepth 1); do
             pandoc "$filepath" -o "$HOME/tmp/$pdffilename" --pdf-engine=xelatex -V geometry:margin=1in -V papersize:a4 -V mainfont:Archivo-Regular --toc --toc-depth=1
             gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook -dPrinted=false -dNOPAUSE -dQUIET -dBATCH -sOutputFile="$HOME/public/pdfs/${pdffilename//\.fullsite/}" "$HOME/tmp/$pdffilename"
         else
-            pandoc "$filepath" -o "$HOME/tmp/$pdffilename" --pdf-engine=xelatex -V geometry:margin=1in -V papersize:a4 -V mainfont:Archivo-Regular
-            gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook -dPrinted=false -dNOPAUSE -dQUIET -dBATCH -sOutputFile="$HOME/public/pdfs/$pdffilename" "$HOME/tmp/$pdffilename"
-        fi 
-    fi
+           pandoc "$filepath" -o "$HOME/tmp/$pdffilename" --pdf-engine=xelatex -V geometry:margin=1in -V papersize:a4 -V mainfont:Archivo-Regular
+           gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook -dPrinted=false -dNOPAUSE -dQUIET -dBATCH -sOutputFile="$HOME/public/pdfs/$pdffilename" "$HOME/tmp/$pdffilename"
+       fi 
+   fi
 done
